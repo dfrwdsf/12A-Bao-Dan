45efc89eb7d9915d0dbd9c3133043ea4b083abdb
12A Bao Dan <85391083+dfrwdsf@users.noreply.github.com>
2025-03-04 09:19:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function login ()
 { let a = username.value
   let b = password.value
  if ( a == "Đango" && b == "02092007")
+
+ { window.open("home.html")
+  document.getElementbyId(compiler).style.display="block"; 
+ }
+ else document.write("Không hợp lệ")
